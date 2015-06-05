5dc66ef6b932825035993b132d5c89a58d97c107
Alexander Ponomarev <ikidoit@gmail.com>
2015-06-05 19:58:20 +0300

    adjustments for heroku

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv'
+import uriUtil from 'mongodb-uri'
 
 var dotenvFilename
+var DB_URL
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
 	dotenvFilename = '.env'
@@ -13,9 +15,19 @@ dotenv.config({
   path: dotenvFilename
 })
 
-export default {
+// for heroku
+// https://github.com/mongolab/mongodb-driver-examples/blob/master/nodejs/mongooseSimpleExample.js
+if (process.env.NODE_ENV == 'production') {
+	DB_URL = uriUtil.formatMongoose(process.env.MONGOLAB_URI)
+} else {
+	DB_URL = process.env.DB_URL
+}
+
+const Config = {
 	admin_login: process.env.ADMIN_LOGIN,
 	admin_password: process.env.ADMIN_PASSWORD,
-	db_url: process.env.DB_URL,
+	db_url: DB_URL,
 	postsPerPage: 5
 }
+
+export default Config
--- models/index.js
+++ models/index.js
@@ -1,5 +1,20 @@
+import mongoose from 'mongoose'
 import config from '../config'
 import post from './post'
 
-var mongoose = require('mongoose');
-mongoose.connect(config.db_url);
+var options
+
+if (process.env.NODE_ENV === 'production') {
+	options = {
+		server: {
+			socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 }
+		}, 
+		replset: {
+			socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 }
+		}
+	}
+} else {
+	options = {}
+}
+
+mongoose.connect(config.db_url, options)
